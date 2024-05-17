22f3700b6267156a6e59baf3d0f89f61bd1c448e
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2024-05-17 13:04:09 +0330

    add report

--- src/main/NavigationStrategy/ExpressNavigation.java
+++ src/main/NavigationStrategy/ExpressNavigation.java
@@ -4,7 +4,7 @@ public class ExpressNavigation implements NavigationStrategy{
     @Override
     public double calculateNavigationPrice(double weight) {
         double price = 3.5 * weight;
-        System.out.println("Current price is " + price + " based on Express Navigation");
+        System.out.println("--- Current price is " + price + " based on Express Navigation ---");
         return price;
     }
 }
